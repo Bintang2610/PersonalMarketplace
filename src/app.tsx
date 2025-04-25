3a6d9438043117d648e8555191353fa03dba9447
Bintang2610 <hanifanbintang06@gmail.com>
2025-04-25 22:05:32 +0700

    update homepage footer

--- src/app.tsx
+++ src/app.tsx
@@ -30,12 +30,18 @@ const App: Component = (props: { children: Element }) => {
     onCleanup(() => window.removeEventListener("scroll", handleScroll));
   });
 
+  const [showText, setShowText] = createSignal(false);
+
+  const [showText2, setShowText2] = createSignal(false);
+
+  const [showText3, setShowText3] = createSignal(false);
+
   return (
     <>
       <nav class={`bg-white fixed w-full z-30 top-0 start-0 border-b border-gray-200 transition-transform duration-300 ${
     showNavbar() ? "translate-y-0" : "-translate-y-full"
   }`}>
-        <div class="max-w-screen-xl mx-auto p-4 relative flex items-center justify-between">
+        <div class="w-auto mx-4 md:mx-6 p-4 relative flex items-center justify-between">
           <A href="/" class="h-full flex items-center z-10">
             <span class="text-lg font-semibold">PersonalStore</span>
           </A>
@@ -57,16 +63,16 @@ const App: Component = (props: { children: Element }) => {
           </div>
           <div class="flex items-center space-x-3 z-10">
             <A href="/profil" class="flex items-center rounded-full px-2 py-2 hover:bg-gray-100 bg-white justify-center text-black text-md">
+              <i class="fa-solid fa-magnifying-glass"></i>
+            </A>
+            <A href="/profil" class="flex items-center rounded-full px-2 py-2 hover:bg-gray-100 bg-white justify-center text-black text-md">
               <i class="fa-regular fa-heart"></i>
             </A>
             <A href="/profil" class="flex items-center rounded-full px-2 py-2 hover:bg-gray-100 bg-white justify-center text-black text-md">
               <i class="fa-regular fa-user"></i>
             </A>
-            <button onClick={toggleMobileMenu} type="button" class="md:hidden p-2 w-10 h-10 text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
-              <svg class="w-5 h-5" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                      d="M1 1h15M1 7h15M1 13h15"/>
-              </svg>
+            <button onClick={toggleMobileMenu} type="button" class="md:hidden px-2 py-1 h-auto justify-center text-gray-500 rounded-lg hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200">
+              <i class="fa-solid fa-bars"></i>
             </button>
           </div>
         </div>
@@ -80,6 +86,105 @@ const App: Component = (props: { children: Element }) => {
       <main>
         <Suspense>{props.children}</Suspense>
       </main>
+
+      <footer class="border-t border-gray-300">
+        <div class="flex flex-col md:space-x-4 md:flex-row text-black px-8 py-8 text-sm text-gray-700">
+          <div class="w-auto md:w-1/4 mb-2 flex-col">
+            <div class="flex items-center justify-between cursor-pointer md:cursor-default" onClick={() => setShowText(!showText())}>
+              <p class="flex items-center text-lg font-medium py-4">
+                <i class="fa-solid fa-location-dot mr-2"></i>Gerai Kami
+              </p>
+              <i
+                class={`fa-solid fa-chevron-down ml-2 transition-transform duration-300 md:hidden ${
+                  showText() ? "rotate-180" : ""
+                }`}
+              ></i>
+            </div>
+            <ul class={`${showText() ? "block" : "hidden md:block"} transition-opacity duration-300`}>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Komplek PJKA 386-388, JL. Jend. Sudirman, Purwokerto Lor, Purwokerto, Sokanegara, Kec. Purwokerto Tim., Kabupaten Banyumas, Jawa Tengah 53115</A>
+              </li>
+            </ul>
+          </div>
+          <div class="w-full md:w-1/4 mb-2 flex-col">
+            <div class="flex items-center justify-between cursor-pointer md:cursor-default" onClick={() => setShowText2(!showText2())}>
+              <p class="flex items-center text-lg font-medium py-4">
+                <i class="fa-solid fa-phone mr-2"></i>Pelayanan
+              </p>
+              <i
+                class={`fa-solid fa-chevron-down ml-2 transition-transform duration-300 md:hidden ${
+                  showText2() ? "rotate-180" : ""
+                }`}
+              ></i>
+            </div>
+            <ul class={`${showText2() ? "block" : "hidden md:block"} space-y-2 transition-opacity duration-300`}>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Minta bantuan</A>
+              </li>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Opsi pembayaran</A>
+              </li>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Pengembalian barang</A>
+              </li>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Panduan perawatan</A>
+              </li>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Kontak kami</A>
+              </li>
+            </ul>
+          </div>
+          <div class="w-full md:w-1/4 mb-2 flex-col">
+            <div class="flex items-center justify-between cursor-pointer md:cursor-default" onClick={() => setShowText3(!showText3())}>
+              <p class="flex items-center text-lg font-medium py-4">
+                <i class="fa-solid fa-user-tie mr-2"></i>Tentang kami
+              </p>
+              <i
+                class={`fa-solid fa-chevron-down ml-2 transition-transform duration-300 md:hidden ${
+                  showText3() ? "rotate-180" : ""
+                }`}
+              ></i>
+            </div>
+            <ul class={`${showText3() ? "block" : "hidden md:block"} space-y-2 transition-opacity duration-300`}>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Tentang Personal Store</A>
+              </li>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Legalitas</A>
+              </li>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Karir</A>
+              </li>
+              <li>
+                <A href="/" class="cursor-pointer hover:text-gray-500">Investor</A>
+              </li>
+            </ul>
+          </div>
+          <div class="w-full md:w-1/4 mb-2 flex-col text-right">
+            <p class="flex items-center justify-end text-lg font-medium py-4"><i class="fa-solid fa-globe mr-2"></i>Indonesia</p>
+          </div>
+        </div>
+        <div class="w-full px-8 py-8">
+          <div class="w-auto flex py-4 flex-row space-x-6 text-sm text-black">
+            <A href="" class="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" class="w-5 text-black inline-block mr-1">
+                <path d="M349.33,69.33H162.67A93.34,93.34,0,0,0,69.33,162.67v186.66A93.34,93.34,0,0,0,162.67,442.67h186.66A93.34,93.34,0,0,0,442.67,349.33V162.67A93.34,93.34,0,0,0,349.33,69.33ZM256,362.67A106.67,106.67,0,1,1,362.67,256,106.76,106.76,0,0,1,256,362.67Zm117.33-192a21.33,21.33,0,1,1,21.34-21.34A21.32,21.32,0,0,1,373.33,170.67ZM256,181.33A74.67,74.67,0,1,0,330.67,256,74.77,74.77,0,0,0,256,181.33Z"/>
+              </svg>
+              <span class="text-lg font-semibold">Instagram</span>
+            </A>
+            <A href="" class="flex items-center">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" class="w-4 h-4 text-black inline-block mr-1" viewBox="0 0 320 512">
+                <path d="M279.14 288l14.22-92.66h-88.91V127.38c0-25.35 12.42-50.06 52.24-50.06H293V6.26S259.5 0 225.36 0C141.09 0 89.09 54.42 89.09 153.1v68.24H0v92.66h89.09V512h107.9V288z"/>
+              </svg>
+              <span class="text-lg font-semibold">facebook</span>
+            </A>
+          </div>
+          <div class="w-auto flex py-4 flex-row border-t border-gray-300 space-x-4 border-d text-sm text-black">
+            <p class="font-semibold text-gray-500">&copy; 2025 Hanifan Bintang</p>
+          </div>
+        </div>
+      </footer>
     </>
   );
 };
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -23,18 +23,75 @@ export default function Home() {
     if (revealDiv) observer.observe(revealDiv);
   });
 
+  let revealDiv2: HTMLDivElement | undefined;
+
+  onMount(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting && revealDiv2) {
+            revealDiv2.classList.remove('opacity-0', 'translate-y-10');
+            revealDiv2.classList.add('opacity-100', 'translate-y-0');
+            observer.unobserve(entry.target); // Berhenti observe setelah muncul
+          }
+        });
+      },
+      { threshold: 0.4 }
+    );
+
+    if (revealDiv2) observer.observe(revealDiv2);
+  });
+
+  let revealSec1: HTMLDivElement | undefined;
+
+  onMount(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting && revealSec1) {
+            revealSec1.classList.remove('opacity-0', 'translate-x-0');
+            revealSec1.classList.add('opacity-100', 'translate-x-10');
+            observer.unobserve(entry.target); // stop observing after reveal
+          }
+        });
+      },
+      { threshold: 0.8 }
+    );
+
+    if (revealSec1) observer.observe(revealSec1);
+  });
+
+  let revealSec2: HTMLDivElement | undefined;
+
+  onMount(() => {
+    const observer = new IntersectionObserver(
+      entries => {
+        entries.forEach(entry => {
+          if (entry.isIntersecting && revealSec2) {
+            revealSec2.classList.remove('opacity-0', 'translate-x-0');
+            revealSec2.classList.add('opacity-100', 'translate-x-10');
+            observer.unobserve(entry.target); // stop observing after reveal
+          }
+        });
+      },
+      { threshold: 0.6 }
+    );
+
+    if (revealSec2) observer.observe(revealSec2);
+  });
+
   const [scrollX, setScrollX] = createSignal(0);
   let containerRef;
 
   const scrollNext = () => {
     if (containerRef) {
-      containerRef.scrollBy({ left: 195, behavior: 'smooth' }); // 195 + gap
+      containerRef.scrollBy({ left: 215, behavior: 'smooth' });
     }
   };
 
   const scrollPrev = () => {
     if (containerRef) {
-      containerRef.scrollBy({ left: -195, behavior: 'smooth' });
+      containerRef.scrollBy({ left: -215, behavior: 'smooth' });
     }
   };
 
@@ -45,15 +102,37 @@ export default function Home() {
     img: `/src/public/images/mockup/${(i % 4) + 1}.png`
   }));
 
+  const [scrollX2, setScrollX2] = createSignal(0);
+  let containerRef2;
+
+  const scrollNext2 = () => {
+    if (containerRef2) {
+      containerRef2.scrollBy({ left: 215, behavior: 'smooth' });
+    }
+  };
+
+  const scrollPrev2 = () => {
+    if (containerRef2) {
+      containerRef2.scrollBy({ left: -215, behavior: 'smooth' });
+    }
+  };
+
+  const products2 = Array.from({ length: 10 }).map((_, i) => ({
+    id: i + 1,
+    title: `Hoodie ${i + 1}`,
+    price: `Rp ${200000 + i * 10000}`,
+    img: `/src/public/images/mockup/${(i % 4) + 1}.png`
+  }));
+
   return (
-    <section class="mx-8 mt-20">
-    <A href=""
+    <section class="mx-8 md:mx-10 mt-20">
+    <A href="block"
       class="relative w-full h-[100vh] bg-cover bg-center md:bg-top flex items-center group"
       style="background-image: url('/src/public/images/mockup/13.png');"
     >
       <div class="absolute inset-0 bg-black/5 transition-opacity duration-300 group-hover:opacity-0"></div>
 
-      <div class="w-full max-w-sm md:max-w-sm lg:max-w-md ml-10 md:ml-20 lg:ml-32 p-4 rounded text-white font-medium">
+      <div class="w-full max-w-sm md:max-w-sm lg:max-w-md ml-6 md:ml-20 lg:ml-32 p-4 rounded text-white font-medium">
         <h2 class="w-full mb-1 text-3xl lg:text-4xl font-medium lg:font-semibold">Hoodie terbaru dari Personal Store</h2>
         <p class="mb-2 text-md">Hoodie 1.1</p>
         <A href="/" class=" text-sm opacity-0 transition-opacity duration-300 group-hover:opacity-100">
@@ -62,7 +141,7 @@ export default function Home() {
       </div>
     </A>
     <div
-      class="relative w-full h-[655px] py-20 flex justify-center items-center"
+      class="relative w-full h-[655px] flex justify-center items-center"
     >
       <div class="w-full flex flex-col justify-center p-4 text-black">
         <h2 class="mb-6 text-3xl lg:text-4xl font-medium lg:font-semibold">
@@ -121,7 +200,120 @@ export default function Home() {
         </A>
       </div>
     </div>
-  </section>
+    <A href=""
+      class="relative w-full h-[90vh] mb-12 bg-cover bg-center md:bg-top text-center justify-center flex items-center group"
+      style="background-image: url('/src/public/images/mockup/10.png');"
+    >
+      <div class="absolute z-0 inset-0 bg-black/15 transition-opacity duration-300 opacity-0 group-hover:opacity-100"></div>
 
+      <div class="w-full z-10 max-w-sm md:max-w-sm lg:max-w-md p-4 rounded text-white font-medium transition-opacity opacity-0 duration-300 group-hover:opacity-100">
+        <h2 class="w-full mb-2 text-3xl lg:text-4xl font-medium lg:font-semibold">Cari hoodie favoritmu di Personal Store</h2>
+          <A href="/" class="text-sm justify-center text-center flex">
+            <div class="flex items-center px-4 py-2 text-black hover:bg-gray-100 bg-white rounded-full">Kunjungi laman <i class="fa-solid fa-arrow-right ml-2"></i></div>
+          </A>
+      </div>
+    </A>
+    <div class="flex mb-12">
+      <A href=""
+        class="relative w-1/2 h-[90vh] bg-cover bg-center md:bg-top flex items-end group"
+        style="background-image: url('/src/public/images/mockup/9.png');"
+      >
+        <div class="absolute inset-0 bg-black/15 transition-opacity duration-300 group-hover:opacity-0"></div>
+
+        <div ref={revealSec1} class="transition-all duration-700 ease-out transform opacity-0 translate-x-0 w-full max-w-[110px] md:max-w-sm lg:max-w-md ml-none md:ml-8 lg:ml-16 py-4 rounded text-white font-medium">
+          <h2 class="w-full text-shadow-lg mb-2 text-3xl lg:text-4xl font-medium lg:font-semibold">Pilihan Pria</h2>
+          <A href="/" class="text-sm opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <div class="flex items-center w-40 text-center justify-center mb-20 px-4 py-2 text-black hover:bg-gray-100 bg-white rounded-full">Kunjungi laman <i class="fa-solid fa-arrow-right ml-2"></i></div>
+          </A>
+        </div>
+      </A>
+      <A href=""
+        class="relative w-1/2 h-[90vh] bg-cover bg-center md:bg-top flex items-start group"
+        style="background-image: url('/src/public/images/mockup/11.png');"
+      >
+        <div class="absolute inset-0 bg-black/15 transition-opacity duration-300 group-hover:opacity-0"></div>
+
+        <div ref={revealSec2} class="transition-all duration-700 ease-out transform opacity-0 translate-x-0 w-full max-w-[110px] md:max-w-sm lg:max-w-md ml-none md:ml-8 lg:ml-16 py-4 rounded text-white font-medium">
+          <h2 class="w-full text-shadow-lg mt-20 mb-2 text-3xl lg:text-4xl font-medium lg:font-semibold">Pilihan Wanita</h2>
+          <A href="/" class="text-sm opacity-0 transition-opacity duration-300 group-hover:opacity-100">
+            <div class="flex items-center w-40 text-center justify-center px-4 py-2 text-black hover:bg-gray-100 bg-white rounded-full">Kunjungi laman <i class="fa-solid fa-arrow-right ml-2"></i></div>
+          </A>
+        </div>
+      </A>
+    </div>
+    <A href=""
+      class="relative w-full h-[60vh] bg-cover bg-center md:bg-top text-center justify-center flex items-center group"
+      style="background-image: url('/src/public/images/mockup/11.png'); background-position: center 30%;"
+    >
+      <div class="absolute z-0 inset-0 bg-black/15 transition-opacity duration-300 opacity-0 group-hover:opacity-100"></div>
+
+      <div class="w-full z-10 max-w-sm md:max-w-sm lg:max-w-md p-4 rounded text-white font-medium transition-opacity opacity-0 duration-300 group-hover:opacity-100">
+        <h2 class="w-full mb-3 text-3xl lg:text-4xl font-medium lg:font-semibold">Kenali gaya yang cocok dengan kamu</h2>
+          <A href="/" class="text-sm justify-center text-center flex">
+            <div class="flex items-center px-4 py-2 text-black hover:bg-gray-100 bg-white rounded-full">Kunjungi laman <i class="fa-solid fa-arrow-right ml-2"></i></div>
+          </A>
+      </div>
+    </A>
+    <div
+      class="relative w-full h-[655px] flex justify-center items-center"
+    >
+      <div class="w-full flex flex-col justify-center p-4 text-black">
+        <h2 class="mb-6 text-3xl lg:text-4xl font-medium lg:font-semibold">
+          Populer di Personal Store
+        </h2>
+
+        <div ref={revealDiv2} class="opacity-0 translate-y-10 transition-all duration-700 overflow-x-auto w-full flex-1">
+          <div class="relative w-full">
+            <button
+              onClick={scrollPrev2}
+              class="absolute z-10 left-0 top-1/2 -translate-y-1/2 bg-white p-2"
+            >
+              <i class="fa-solid fa-chevron-left"></i>
+            </button>
+
+            <div
+              ref={el => (containerRef2 = el)}
+              class="overflow-x-hidden"
+            >
+              <div class="flex space-x-5 w-fit">
+                {products2.map(product2 => (
+                  <a
+                    href="/"
+                    class="w-[195px] flex-shrink-0 flex flex-col overflow-hidden bg-white"
+                  >
+                    <img
+                      src={product2.img}
+                      alt={product2.title}
+                      class="w-full h-[240px] object-cover border"
+                    />
+                    <div class="p-2 text-left text-black">
+                      <p class="text-md font-semibold">{product2.title}</p>
+                      <p class="text-sm">{product2.price}</p>
+                    </div>
+                  </a>
+                ))}
+              </div>
+            </div>
+
+            <button
+              onClick={scrollNext2}
+              class="absolute z-10 right-0 top-1/2 -translate-y-1/2 bg-white p-2"
+            >
+              <i class="fa-solid fa-chevron-right"></i>
+            </button>
+          </div>
+        </div>
+        <A
+          href="/"
+          class="text-sm mt-4"
+        >
+          <div class="flex items-center w-fit text-black rounded-full">
+            <p class="hover:underline">Lihat selengkapnya</p>
+            <i class="fa-solid fa-chevron-right ml-1"></i>
+          </div>
+        </A>
+      </div>
+    </div>
+  </section>
   );
 }
